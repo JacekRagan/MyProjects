d353494c5ed0946bd95d7b63f44acc058358124a
Jacek Ragan <90776401+JacekRagan@users.noreply.github.com>
2022-02-24 21:34:03 +0100

    Add files via upload

---
+++ speedtest.py
@@ -0,0 +1,16 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import pyautogui as pya
+driver = webdriver.Chrome('C:\Program Files (x86)\chromedriver.exe')
+driver.get('https://10fastfingers.com/typing-test/english')
+time.sleep(2)
+cookie = driver.find_element(By.XPATH,'/html/body/div[1]/div/div[4]/div[1]/div[2]/button[4]').click()
+time.sleep(1)
+while True:
+    slowko = driver.find_element(By.CSS_SELECTOR,'#row1 > span.highlight').text
+    wpisz = driver.find_element(By.XPATH,'/html/body/div[5]/div/div[4]/div/div[1]/div[7]/div[2]/div/div[1]/input')
+    wpisz.send_keys(slowko)
+    pya.press('space')
+            
